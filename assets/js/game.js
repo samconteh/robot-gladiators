69e5b85da2116b4a2a1801f002ec930c48b8a632
sam conteh <sambujangconteh4@gmail.com>
2022-07-28 18:53:45 -0400

    add console.log

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,8 @@
-window.alert("This is an alert! JavaScript is running!");
+function fight() {
+  window.alert("The fight has begun!");
+}
+// fight();
+var playerName = "TOny the Robot";
+var playerName = window.prompt("What is your robot's name?");
+// Note the lack of quotation marks around playerName
+console.log(playerName);
